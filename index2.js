16417ce9c5ae9b59826f7034f77b9503ea207465
anjasmara <anjasmaradwi@refactory.id>
2022-07-17 13:34:24 +0700

    learn debounce,distinctUntilChange, reduce

--- index2.js
+++ index2.js
@@ -1,7 +1,23 @@
-const { interval, throttleTime, map, subscribe, Observable } = rxjs;
+const { 
+    from,
+    interval, 
+    throttleTime,
+    map, 
+    subscribe, 
+    Observable, 
+    Subject,
+    fromEvent,
+    debounceTime,
+    distinctUntilChanged,
+    filter,
+    reduce
+} = rxjs;
 
+const inputUsername = document.getElementById('inputUsername');
 const observableData = interval(1000)
 const observableSubject = new Subject()
+const valueCollect = from([1,2,3,4,5]);
+const observable = from([10, 20, 30]);
 
 const valObservable = {
     next: (value) => {
@@ -45,3 +61,30 @@ observableSubject.subscribe({
 
 observableSubject.next('A new data Piece')
 observableSubject.complete()
+
+/* use Debounce Time */
+const eventInput = fromEvent(inputUsername, 'input')
+function eventInputTrigger(){
+    eventInput.pipe(
+        map((value)=> value.target.value),
+        debounceTime(2000),
+        distinctUntilChanged()
+    )
+    .subscribe((value)=>{
+        console.log('change input')
+        console.log(value)
+    })
+}
+
+/* use reduce Time */
+function reduceUsing(){
+   valueCollect.pipe(
+        reduce((total, current) => {return total+current},0)
+    ). subscribe((value)=>{
+        console.log('value ssss')
+        console.log(value)
+    })
+}
+
+reduceUsing()
+eventInputTrigger();
